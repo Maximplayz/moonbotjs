08e46b034a0cea90f0c1c31a8bc9d3a200bae224
unknown <maxmax3d12@gmail.com>
2018-02-28 20:05:12 +0100

    Updating

--- commands/help.js
+++ commands/help.js
@@ -10,8 +10,10 @@ module.exports.run = async (bot, message, args) => {
             message.channel.send('__**Bot Owner Only Commands**__\n\n``moon.die`` => **Terminates connection to discord**\n``moon.hook <title> <message> [color] [avatar]`` => **Creates a Webhook**\n```moon.eval <code>`` => **Evaluates Javascript Code**\n``moon.username <new_name>`` => **Changes the Bots Global Username**')
         }
 
+        if (args[0] === '') {
         message.channel.send('__**Aviable Categories**__\n\n``general (moon.help general)`` => **General Command Help**\n``botowner (moon.help botowner)`` => **Bot Owner Command Help**')
-
+        }
+        
 }
 
 module.exports.help = {
