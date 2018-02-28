3d0fc0c277de57c2ce29ef91ff083944fe4350ec
unknown <maxmax3d12@gmail.com>
2018-02-28 20:00:48 +0100

    Updating

--- commands/help.js
+++ commands/help.js
@@ -2,21 +2,16 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 
-    var HELPEMBED = new Discord.RichEmbed()
-    .setTitle('``All Bot Commands``')
-    .setColor('ff0000')
-    .addField('moon.ping', '**Gets the bots ping**')
-    .addField('moon.userinfo', '**Gets info about the Mentioned user**\n**Usage:** ``moon.userinfo <mention>``')
-    .addField('moon.die', '**Terminates Connection to Discord ``(Bot Owner Only)``**')
-    .addField('moon.username', '**Changes the bots usename ``(Bot Owner Only)``**\n**Usage:** ``moon.username <newName>``')
-    .addField('moon.say', '**Lets the bot Repeat what you input**\n**Usage:** ``moon.say <message>``')
-    .addField('moon.shorten', '**Shortens a link\nUsage: ** ``moon.shorten <URL> [Title] <- The title is optional``')
-    .addField('moon.hook', '**Sends a COOL webhook ``(Bot Owner only)``**\n**Usage: **``moon.hook <title> <message> [HEXcolor] [avatarURL]``\n<> is required\n [] is optional')
-    .addField('moon.weather', '**Checks the Weather for the Specified City\nUsage: ** ```moon.weather <city>``')
-    .addField('moon.eval', '**Evaluate Javascript code ``(Bot Owner Only)``\nUsage: ``moon.eval <code>``')
-    .setFooter('Moonbot coded by LSD#3367', bot.user.displayAvatarURL)
-    message.channel.send(HELPEMBED)
-    
+    	if (args[0] === 'general') {
+            message.channel.send('__**General Commands**__\n\n``moon.userinfo <mention>`` => **Shows info about the Mentioned user.**\n``moon.weather <city>`` => **Shows weather for a specific City**\n``moon.shorten <url> [title]`` => **Shortens a URL with optional title**\n``moon.ping`` => **Shows the bots ping + memory usage**')
+        }
+
+        if (args[0] === 'botowner') {
+            message.channel.send('__**Bot Owner Only Commands**__\n\n``moon.die`` => **Terminates connection to discord**\n``moon.hook <title> <message> [color] [avatar]`` => **Creates a Webhook**\n```moon.eval <code>`` => **Evaluates Javascript Code**\n``moon.username <new_name>`` => **Changes the Bots Global Username**')
+        }
+
+        message.channel.send('__**Aviable Categories**__\n\n``general (moon.help general)`` => **General Command Help**\n``botowner (moon.help botowner)`` => **Bot Owner Command Help**')
+
 }
 
 module.exports.help = {
